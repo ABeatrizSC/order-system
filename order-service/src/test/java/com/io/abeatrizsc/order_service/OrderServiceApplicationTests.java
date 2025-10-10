f54c6911478c602339933eef7b558cf31c201fa7
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2025-10-10 00:13:45 -0300

    initial project structure

---
+++ order-service/src/test/java/com/io/abeatrizsc/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.abeatrizsc.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
